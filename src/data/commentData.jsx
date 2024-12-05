de76677ad7c4384702a996e41437ec5042d3ac2e
nohasalim <noha.fcs@gmail.com>
2024-12-05 17:37:36 +0300

    modify-images-path

--- src/data/commentData.jsx
+++ src/data/commentData.jsx
@@ -1,13 +1,13 @@
 export const commentData = [
   {
     id: "1",
-    image: "public/Images/image-amyrobson.png",
+    image: "Images/image-amyrobson.png",
     username: "amyrobson",
     text: "Hello world! How are you?",
     replies: [
       {
         id: "2",
-        image: "public/Images/image-juliusomo.png",
+        image: "Images/image-juliusomo.png",
         username: "juliusomo",
         text: "Hey, I am fine, wau?",
         replies: [],
@@ -17,13 +17,13 @@ export const commentData = [
 
   {
     id: "3",
-    image: "public/Images/image-maxblagun.png",
+    image: "Images/image-maxblagun.png",
     username: "maxblagun",
     text: "Hello, I am good, How are you?",
     replies: [
       {
         id: "4",
-        image: "public/Images/image-ramsesmiron.png",
+        image: "Images/image-ramsesmiron.png",
         username: "ramsesmiron",
         text: "Life sucks man!",
         replies: [],
